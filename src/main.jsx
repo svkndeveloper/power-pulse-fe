88b9471e81d84eea8bbd5c634ea3e07992616ba2
SKhrebtan <shrebtan@gmail.com>
2024-01-26 09:22:28 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/power-pulse-fs">
+        <BrowserRouter basename="/power-pulse-fe">
           <App />
         </BrowserRouter>
       </PersistGate>
